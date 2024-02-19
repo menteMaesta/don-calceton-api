195b1b43e1661827c9211282a9a583be0393844c
menteMaesta <paulagomenr@gmail.com>
2024-02-19 16:38:16 -0600

    Add static server, update token to send expiration date

--- app/controllers/token_controller.ts
+++ app/controllers/token_controller.ts
@@ -7,10 +7,7 @@ export default class TokenController {
     const { email, password } = await request.validateUsing(loginValidator)
     const user = await User.verifyCredentials(email, password)
     const token = await User.accessTokens.create(user)
-    return {
-      type: 'bearer',
-      value: token.value!.release(),
-    }
+    return token
   }
 
   async logout({ auth, response }: HttpContext) {
