7f0b5b6170125d28051727b3bf7d4b58c654b1ec
menteMaesta <paulagomenr@gmail.com>
2024-07-29 17:59:37 -0600

    Update show endpoint for orders to contain all necesary data

--- app/models/order.ts
+++ app/models/order.ts
@@ -1,5 +1,9 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { BaseModel, column, hasMany, belongsTo } from '@adonisjs/lucid/orm'
+import type { HasMany, BelongsTo } from '@adonisjs/lucid/types/relations'
+import OrderImage from '#models/order_image'
+import Customization from '#models/customization'
+import Variant from '#models/variant'
 
 export default class Order extends BaseModel {
   @column({ isPrimary: true })
@@ -20,4 +24,13 @@ export default class Order extends BaseModel {
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime
+
+  @hasMany(() => OrderImage)
+  declare images: HasMany<typeof OrderImage>
+
+  @belongsTo(() => Customization)
+  declare customization: BelongsTo<typeof Customization>
+
+  @belongsTo(() => Variant)
+  declare variant: BelongsTo<typeof Variant>
 }
